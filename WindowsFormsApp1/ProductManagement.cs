5c606959b79fe25afea5f457e3f13700293657f3
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-04 19:17:18 -0400

    Added component model

--- WindowsFormsApp1/ProductManagement.cs
+++ WindowsFormsApp1/ProductManagement.cs
@@ -2,6 +2,9 @@
 {
     partial class ProductManagement
     {
-
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
     }
 }

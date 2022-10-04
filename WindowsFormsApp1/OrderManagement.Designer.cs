d227a882594707fe58c8bda5760d16c84380a0de
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-04 18:39:26 -0400

    Initiated Order Management 001

--- WindowsFormsApp1/OrderManagement.Designer.cs
+++ WindowsFormsApp1/OrderManagement.Designer.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// 1 - Initiated Order Management
+
+namespace WindowsFormsApp1
+{
+    partial class OrderManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+    }
+}

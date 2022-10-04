7cab5916b7a6745eebae4f16d1ee1c346e50409c
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-04 18:44:40 -0400

    label1 & label2 007

--- WindowsFormsApp1/OrderManagement.Designer.cs
+++ WindowsFormsApp1/OrderManagement.Designer.cs
@@ -1,4 +1,4 @@
-﻿// panel1 6
+﻿// label1 & label2 007
 
 namespace WindowsFormsApp1
 {
@@ -75,7 +75,25 @@ namespace WindowsFormsApp1
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(555, 105);
             this.panel1.TabIndex = 0;
-
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Customer Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
 
         }
 

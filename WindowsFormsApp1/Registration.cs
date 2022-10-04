647c7b166e5d8d3d184482f26681e6d79925cb31
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-10-03 23:29:55 -0400

    initialized registration form

--- WindowsFormsApp1/Registration.cs
+++ WindowsFormsApp1/Registration.cs
@@ -1,9 +1,44 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
 namespace WindowsFormsApp1
 {
     public partial class Registration : Form
     {
-       
+        private SqlCommand cmd;
+        private SqlDataReader dr;
+        private SqlConnection cn;
+
+        public Registration()
+        {
+            
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+        
+        }
       
     }
 }

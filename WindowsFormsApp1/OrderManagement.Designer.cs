a07f6863d7dbe445b6a632cfa1229b3b6355c07b
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-04 18:41:25 -0400

    Dispose method 3

--- WindowsFormsApp1/OrderManagement.Designer.cs
+++ WindowsFormsApp1/OrderManagement.Designer.cs
@@ -1,4 +1,4 @@
-﻿// added comments on order management 2
+﻿// Dispose method 3
 
 namespace WindowsFormsApp1
 {
@@ -13,7 +13,14 @@ namespace WindowsFormsApp1
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
     }
 }

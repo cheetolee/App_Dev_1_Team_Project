dd1ea2b506df2a785bf001c8c2cab88fc2392727
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-04 18:40:28 -0400

    added comments on order management 2

--- WindowsFormsApp1/OrderManagement.Designer.cs
+++ WindowsFormsApp1/OrderManagement.Designer.cs
@@ -1,4 +1,4 @@
-﻿// 1 - Initiated Order Management
+﻿// added comments on order management 2
 
 namespace WindowsFormsApp1
 {
@@ -8,6 +8,12 @@ namespace WindowsFormsApp1
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
 
     }
 }

accd42a5c7b01ce673e298216baa9cc9a71832d9
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-10-05 01:57:24 -0400

    edit labels

--- WindowsFormsApp1/Registration.Designer.cs
+++ WindowsFormsApp1/Registration.Designer.cs
@@ -1,67 +1,85 @@
-﻿namespace WindowsFormsApp1
+﻿amespace WindowsFormsApp1
 {
     partial class Registration
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.txtPwd = new System.Windows.Forms.TextBox();
-            this.btnRegister = new System.Windows.Forms.Button();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.txtConfirmPwd = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.TextBox txtPwd;
-        private System.Windows.Forms.Button btnRegister;
-        private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.TextBox txtConfirmPwd;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label6;
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.label1 = new System.Windows.Forms.Label();
+        this.label2 = new System.Windows.Forms.Label();
+        this.txtUsername = new System.Windows.Forms.TextBox();
+        this.txtPwd = new System.Windows.Forms.TextBox();
+        this.btnRegister = new System.Windows.Forms.Button();
+        this.btnLogin = new System.Windows.Forms.Button();
+        this.txtConfirmPwd = new System.Windows.Forms.TextBox();
+        this.label3 = new System.Windows.Forms.Label();
+        this.txtEmail = new System.Windows.Forms.TextBox();
+        this.label4 = new System.Windows.Forms.Label();
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.label5 = new System.Windows.Forms.Label();
+        this.textBox2 = new System.Windows.Forms.TextBox();
+        this.label6 = new System.Windows.Forms.Label();
+        this.SuspendLayout();
+        // 
+        // label1
+        // 
+        this.label1.AutoSize = true;
+        this.label1.Location = new System.Drawing.Point(39, 48);
+        this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size(88, 13);
+        this.label1.TabIndex = 0;
+        this.label1.Text = "Enter User Name";
+        // 
+        // label2
+        // 
+        this.label2.AutoSize = true;
+        this.label2.Location = new System.Drawing.Point(39, 84);
+        this.label2.Name = "label2";
+        this.label2.Size = new System.Drawing.Size(81, 13);
+        this.label2.TabIndex = 1;
+        this.label2.Text = "Enter Password";
+
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox txtUsername;
+    private System.Windows.Forms.TextBox txtPwd;
+    private System.Windows.Forms.Button btnRegister;
+    private System.Windows.Forms.Button btnLogin;
+    private System.Windows.Forms.TextBox txtConfirmPwd;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox txtEmail;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label6;
+}
 }

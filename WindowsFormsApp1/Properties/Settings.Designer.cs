962ca42ce276aeafa4cfd43d67c0bc61b89c9623
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-05 22:06:04 -0400

    using system runtime setting

--- WindowsFormsApp1/Properties/Settings.Designer.cs
+++ WindowsFormsApp1/Properties/Settings.Designer.cs
@@ -2,4 +2,8 @@
 
 namespace WindowsFormsApp1.Properties
 {
+
+
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
 }

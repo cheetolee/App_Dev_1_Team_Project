1c26b34352a479b0b1cbdddd78dedcf46748c664
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-05 22:10:06 -0400

    using configuration scope setting

--- WindowsFormsApp1/Properties/Settings.Designer.cs
+++ WindowsFormsApp1/Properties/Settings.Designer.cs
@@ -10,5 +10,19 @@ namespace WindowsFormsApp1.Properties
     {
 
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+        public static Settings Default
+        {
+            get
+            {
+                return defaultInstance;
+            }
+        }
+
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\database.mdf;" +
+            "Integrated Security=True;Connect Timeout=30")]
     }
 }

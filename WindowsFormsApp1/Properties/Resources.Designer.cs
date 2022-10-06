d0005d0a1b5a949d22b3de7fc687585e24138017
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-05 21:54:03 -0400

    work on attributes

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -2,5 +2,7 @@
 
 namespace WindowsFormsApp1.Properties
 {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
 }

30a611b01d6c50a5773e70fe1763ab82aa293fc9
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-18 18:11:06 -0400

    Created MainWindow XAML.cs file

--- Red Inventory Management/Views/MainWindow.xaml.cs
+++ Red Inventory Management/Views/MainWindow.xaml.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.Windows;
+using BusinessLayer;
+using Inventory_Management.Notifications;
+using Inventory_Management.ViewModel;
+
+namespace Inventory_Management.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel(this);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            NotificationProvider.Close();
+        }
+    }
+}

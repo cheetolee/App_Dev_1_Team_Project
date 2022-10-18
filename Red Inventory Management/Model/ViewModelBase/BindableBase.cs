1acae51263bc3b9146f2008224537d265c74a1bc
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-18 17:57:01 -0400

    005 - Red Inventory Management --> Model --> ViewModelBase --> BindableBase.cs

--- Red Inventory Management/Model/ViewModelBase/BindableBase.cs
+++ Red Inventory Management/Model/ViewModelBase/BindableBase.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Inventory_Management.Model
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        protected virtual void SetProperty<T>(ref T member, T val, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return;
+            member = val;
+            RaisePropertyChanged(propertyName);
+        }
+    }
+}

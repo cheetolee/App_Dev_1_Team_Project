da90b665a3bff84c351c5d07f040232ba9223263
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-10-18 20:34:31 -0400

    LoginWindow & NewUSerWindow xaml

--- Red Inventory Management/Views/Login/LoginWindow.xaml.cs
+++ Red Inventory Management/Views/Login/LoginWindow.xaml.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Windows;
+using Inventory_Management.ViewModel;
+
+namespace Inventory_Management.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            LoginViewModel context = new LoginViewModel();
+            context.LoginWindow = this;
+            this.DataContext = context;
+        }
+    }
+}

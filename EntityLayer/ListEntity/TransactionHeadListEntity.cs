138a2c2a71ccc0740565418566214b315f02f9e6
MdIbrahimUllah <jeasan0007@gmail.com>
2022-10-26 21:53:58 -0400

    Modified entity layer

---
+++ EntityLayer/ListEntity/TransactionHeadListEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EntityLayer
+{
+    public class TransactionHeadListEntity : IComparable<TransactionHeadListEntity>
+    {
+        public TransactionHeadListEntity() { }
+        public TransactionHeadListEntity(TransactionHeadEntity head, UserEntity user)
+            :this()
+        {
+            Head = head;
+            User = user;
+        }
+        public TransactionHeadListEntity(TransactionHeadEntity head, UserEntity user, decimal listVariable)
+            : this(head,user)
+        {
+            ListVariable = listVariable;
+        }
+
+        public TransactionHeadEntity Head { get; set; }
+        public UserEntity User { get; set; }
+
+        public decimal ListVariable { get; set; }
+
+        public int CompareTo(TransactionHeadListEntity other)
+        {
+            if (Head == null)
+                return User.CompareTo(other.User);
+            return Head.Date.CompareTo(other.Head.Date);
+        }
+    }
+}
---
+++ EntityLayer/TableEntity/UserEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace EntityLayer
+{
+    [Table("Users")]
+    public class UserEntity
+    {
+        public UserEntity() { }
+
+        public UserEntity(string user, string pw) { Username = user; Password = pw; }
+
+        [Key]
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public int CompareTo(UserEntity other)
+        {
+            var compare1 = Username ?? string.Empty;
+            var compare2 = other.Username ?? string.Empty;
+            return compare1.CompareTo(compare2);
+        }
+    }
+}
